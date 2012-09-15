40333009d455a40e2a9a900e4e73550fa5de7788
SND\AdamDriscoll_cp <SND\AdamDriscoll_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2012-09-15 18:16:41 +0000

    Removed favorites button and updated the manifest.
    git-svn-id: https://perspectives.svn.codeplex.com/svn@99711 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ PerspectivesVS2012/Test/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.Test
+{
+    static class GuidList
+    {
+        public const string guidTestPkgString = "13ecdaa3-1bec-4e0a-9314-b74d1a09ca7a";
+        public const string guidTestCmdSetString = "3283c5e2-38b1-4dce-a46c-6ff1535cf585";
+
+        public static readonly Guid guidTestCmdSet = new Guid(guidTestCmdSetString);
+    };
+}
