0548306b15a52948b4eb63cfb1cbc0f121ed2d65
SND\AdamDriscoll_cp <SND\AdamDriscoll_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2012-09-15 17:51:35 +0000

    Added a branch for VS2012 support. Reworked the toolbar. Removed the buggy menu items.
    git-svn-id: https://perspectives.svn.codeplex.com/svn@99710 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ PerspectivesVS2012/Perspectives/FavoritePerspectives.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace AdamDriscoll.Perspectives
+{
+    public class FavoritePerspectives 
+    {
+        private static string favPath;
+
+        static FavoritePerspectives()
+        {
+            favPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Perspectives\\perspectives.favorites.xml");
+
+            var info = new FileInfo(favPath);
+
+            if (!info.Directory.Exists)
+            {
+                info.Directory.Create();
+            }
+
+        }
+
+        public FavoritePerspectives()
+        {
+            MyFavorites = new List<Favorite>();
+        }
+
+        public void Save()
+        {
+            TextWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(favPath);
+                var s = new XmlSerializer(typeof(List<Favorite>));
+                s.Serialize(writer, MyFavorites);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to save perspectives file!", "IO Failure", MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer.Dispose();
+                }
+            }
+        }
+
+        public void Load()
+        {
+            TextReader reader = null;
+            try
+            {
+                if (File.Exists(favPath))
+                {
+                    try
+                    {
+                        reader = new StreamReader(favPath);
+                        var s = new XmlSerializer(typeof(List<Favorite>));
+                        MyFavorites = (List<Favorite>)s.Deserialize(reader);
+                    }
+                    catch
+                    {
+                        //Supports backwards compatibility
+                        reader = new StreamReader(favPath);
+                        var s = new XmlSerializer(typeof(List<string>));
+                        var names = (List<string>)s.Deserialize(reader);
+
+                        int i = 1;
+                        foreach (var name in names)
+                        {
+                            MyFavorites = new List<Favorite>();
+                            MyFavorites.Add(new Favorite { Name = name, Ordinal = i });
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MyFavorites = new List<Favorite>();
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+            }
+        }
+
+        public List<Favorite> MyFavorites { get; private set; }
+    }
+
+    public class Favorite
+    {
+        public string Name { get; set; }
+        public int Ordinal { get; set; }
+    }
+}
---
+++ PerspectivesVS2012/Perspectives/Perspectives.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using EnvDTE;
+
+namespace AdamDriscoll.Perspectives
+{
+    /// <summary>
+    /// Interaction logic for MyControl.xaml
+    /// </summary>
+    public partial class PerspectivesControl : UserControl
+    {
+        public PerspectivesControl()
+        {
+            InitializeComponent();
+        }
+
+        public DTE Dte { get; set; }
+        public void SetPerspectives(IEnumerable<Perspective> perspectives)
+        {
+            lstPerspectives.ItemsSource = perspectives;
+        }
+
+        public void RefreshUi()
+        {
+            if (Dte == null) return;
+            var perspective = new Perspective(Dte);
+            SetPerspectives(perspective.GetPerspectives());
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                var view = (sender as Button).DataContext as Perspective;
+
+                if (view != null)
+                {
+                    view.Apply();
+
+                    RefreshUi();
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                var view = (sender as Button).DataContext as Perspective;
+
+                if (view != null)
+                {
+                    view.Update();
+
+                    RefreshUi();
+                }
+            }
+        }
+
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                var view = (sender as Button).DataContext as Perspective;
+
+                if (view != null)
+                {
+                    view.Delete();
+                    RefreshUi();
+                }
+            }
+        }
+
+        private void Fav_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                var view = (sender as Button).DataContext as Perspective;
+
+                if (view != null)
+                {
+                    view.Favorite = !view.Favorite;
+                    RefreshUi();                        
+                }
+            }
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshUi();
+        }
+
+    }
+}
